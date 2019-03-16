1b1038a357a2f87a910c7b2b3eb441a3bc59273a
Larry Charbonneau IV <larrycharbonneau4@gmail.com>
2019-03-16 17:58:23 -0400

    changed default action to addport, added the option to add a service and added an xml template for the service

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'larrycharbonneau4@gmail.com'
 license 'GPL-3.0'
 description 'Installs/Configures lc4_firewalld'
 long_description 'Installs/Configures lc4_firewalld'
-version '0.1.5'
+version '0.2.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
--- resources/firewalld_manager.rb
+++ resources/firewalld_manager.rb
@@ -1,20 +1,42 @@
 # To learn more about Custom Resources, see https://docs.chef.io/custom_resources.html
 property :portnumber, String, name_attribute: true
+property :service_name, String, name_attribute: true
+property :protocol, String
+property :description, String
+
 
 resource_name 'firewalld_manager'
 
-default_action :create
+default_action :addport
 
-action :create do
+action :addport do
   execute 'add service to firewalld' do
     command "firewall-cmd --permanent --add-port=#{new_resource.portnumber}"
     action :run
-    not_if "firewall-cmd --zone=public --list-ports | grep #{new_resource.portnumber}"
+    not_if { "firewall-cmd --zone=public --list-ports | grep #{new_resource.portnumber}" }
   end
 end
 action :reload do
   execute 'Reload firewalld' do
     command 'firewall-cmd --reload'
     action :run
+  end
+end
+action :addservice do
+  template "/etc/firewalld/services/#{new_resource.service_name}.xml" do
+    source 'service.xml.erb'
+    mode '0644'
+    variables(serivce_name: new_resource.service_name,
+              protocol: new_resource.protocol,
+              portnumber: new_resource.portnumber,
+              description: new_resource.description)
+    not_if File.exist?("/etc/firewalld/services/#{new_resource.service_name}.xml")
+  end
+
+  execute 'Add service to firewalld' do
+    command "firewall-cmd --permanent --add-service=#{new_resource.service_name}"
+    action :run
+    not_if { "firewall-cmd --zone=public --list-services | grep #{new_resource.service_name}"}
+    not_if { "firewall-cmd --zone=public --list-ports | grep #{new_resource.portnumber}" }
   end
 end
